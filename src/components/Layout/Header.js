10c832b1f20012d74492e8a2987197acc2ffd310
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-05-01 12:33:08 +0530

    header

---
+++ src/components/Layout/Header.js
@@ -0,0 +1,19 @@
+import React from "react";
+import mealImage from "../../assets/meals.jpg";
+import classes from "./Header.module.css";
+
+const Header = (props) => {
+  return (
+    <React.Fragment>
+      <header className={classes.header}>
+        <h1>Restaurant</h1>
+        <button>Cart</button>
+        <button>Cart</button>
+      </header>
+      <div className={classes["main-image"]}>
+        <img src={mealImage} alt="A table full of delicious food!" />
+      </div>
+    </React.Fragment>
+  );
+};
+export default Header;
