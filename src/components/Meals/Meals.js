9d39b5b936055686c5047b312f3c38dd6bfad6e9
Sanjay-3797 <sanjayg.ac.in@gmail.com>
2023-05-01 20:58:15 +0530

    meal list added

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,10 +1,12 @@
 import React from "react";
 import MealSummary from "./MealsSummury";
+import AvailableMeals from "./AvailableMeals";
 
 const Meals = () => {
   return (
     <React.Fragment>
       <MealSummary />
+      <AvailableMeals />
     </React.Fragment>
   );
 };
